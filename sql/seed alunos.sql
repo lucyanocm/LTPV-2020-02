e16fd7fa503e619906d20f8b5a046392f64a0554
Lucyano Campos <lucyanocm@gmail.com>
2020-10-30 18:07:18 -0300

    Create seed alunos.sql
    Adicionado script para semear registro de alunos.

---
+++ sql/seed alunos.sql
@@ -0,0 +1,11 @@
+INSERT INTO Alunos (Nome, SobreNome, DataNascimento, Matricula, Observacoes)
+VALUES ('Fulano', 'da Silva', '2000-01-01', '0800001', 'Exelente aluno');
+
+INSERT INTO Alunos (Nome, SobreNome, DataNascimento, Matricula, Observacoes)
+VALUES ('Ayrton', 'Senna da Silva', '1960-03-21', '0800002', 'Exelente aluno, porém é muito apressadinho.');
+
+INSERT INTO Alunos (Nome, SobreNome, DataNascimento, Matricula, Observacoes)
+VALUES ('Anderson', 'da Silva', '1975-04-14', '0800003', 'Se eu fosse professor dele não ousaria contrariá-lo.');
+
+INSERT INTO Alunos (Nome, SobreNome, DataNascimento, Matricula, Observacoes)
+VALUES ('Santos', 'Dumont', '1873-07-20', '0800004', 'Bom aluno, porém voa um pouco durante a aula.');
